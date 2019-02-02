531414eec718e651aed80a742647a005fcb03657
Shahar Yogev <37019685+shaharyogev@users.noreply.github.com>
2019-02-02 16:02:45 +0200

    bmp active

--- server.php
+++ server.php
@@ -17,9 +17,6 @@
 			<input type='submit' name='submit'>
 		</form>
 
-
-
-
 <?php
 
 function console( $data ) {
@@ -30,17 +27,130 @@ function console( $data ) {
     echo "<script>console.log( 'Debug Objects: " . $output . "' );</script>";
 }
 
+$colorsToShowNum = 5;
+$colors = array();
 
-function dominantColors( $img){
-	$fp = fopen($img, 'rb');
-	console($fp);
-	$size = filesize($img);
-	$data = fread($fp, $size);
-	fclose($fp);
-	$encoded = bin2hex($data);
-	console($encoded);
-	echo $encoded;
+function colorsInBmp($p_sFile) { 
 
+	//    Load the image into a string 
+	$file    =    fopen($p_sFile,"rb"); 
+	$read    =    fread($file,10); 
+	while(!feof($file)&&($read<>"")) 
+			$read    .=    fread($file,1024); 
+	
+	$temp    =    unpack("H*",$read); 
+	$hex    =    $temp[1]; 
+	$header    =    substr($hex,0,108); 
+	
+	
+	//    Process the header 
+	//    Structure: http://www.fastgraph.com/help/bmp_header_format.html 
+	
+	if (substr($header,0,4)=="424d") //BMP header
+	{ 
+			//    Cut it in parts of 2 bytes 
+			$header_parts    =    str_split($header,2); 
+			
+			//    Get the width        4 bytes 
+			$width            =    hexdec($header_parts[19].$header_parts[18]); 
+			
+			//    Get the height        4 bytes 
+			$height            =    hexdec($header_parts[23].$header_parts[22]); 
+			
+			//    Unset the header params 
+			unset($header_parts); 
+	} 
+	
+	//    Define starting X and Y 
+	$x                =    0; 
+	$y                =    1; 
+	
+	//    Create newimage 
+	//$image            =    imagecreatetruecolor($width,$height); // GD library cant be used
+	
+	//    Grab the body from the image 
+	$body            =    substr($hex,108); 
+	//    Calculate if padding at the end-line is needed 
+	//    Divided by two to keep overview. 
+	//    1 byte = 2 HEX-chars 
+	$body_size        =    (strlen($body)/2); 
+	$header_size    =    ($width*$height); 
+
+	//    Use end-line padding? Only when needed 
+	$usePadding        =    ($body_size>($header_size*3)+4); 
+	
+	//    Using a for-loop with index-calculation instated of str_split to avoid large memory consumption 
+	//    Calculate the next DWORD-position in the body 
+	for ($i=0;$i<$body_size;$i+=3) 
+	{ 
+			//    Calculate line-ending and padding 
+			if ($x>=$width) 
+			{ 
+					//    If padding needed, ignore image-padding 
+					//    Shift i to the ending of the current 32-bit-block 
+					if ($usePadding) 
+							$i    +=    $width%4; 
+					
+					//    Reset horizontal position 
+					$x    =    0; 
+					
+					//    Raise the height-position (bottom-up) 
+					$y++; 
+					
+					//    Reached the image-height? Break the for-loop 
+					if ($y>$height) 
+							break; 
+			} 
+							
+
+			//    Calculation of the RGB-pixel (defined as BGR in image-data) 
+			//    Define $i_pos as absolute position in the body 
+			$i_pos    =    $i*2; 
+			$r        =    hexdec($body[$i_pos+4].$body[$i_pos+5]); 
+			$g        =    hexdec($body[$i_pos+2].$body[$i_pos+3]); 
+			$b        =    hexdec($body[$i_pos].$body[$i_pos+1]); 
+			
+			// Combine the RGB to a string
+			$tempString = ''.$r.','.$g.','.$b.'';
+			
+			// Insert the strings to array
+			$colors[] = $tempString;
+			
+			//    Calculate and draw the pixel 
+			//$color    =    imagecolorallocate($image,$r,$g,$b); // GD library cant be used
+			//imagesetpixel($image,$x,$height-$y,$color); // GD library cant be used
+			
+			//    Raise the horizontal position 
+			$x++; 
+	} 
+	
+	//    Unset the body / free the memory 
+	unset($body); 
+	
+
+	// Count the number for each color
+	$allTheColors = array_count_values($colors);
+	
+	// Sort the array higher values first
+	arsort($allTheColors);
+	
+	//Slice the top 5 dominant colors
+	$result = array_slice($allTheColors, 0,5,true);
+
+	//Echo to the screen
+	foreach($result as $key => $val){
+		echo '<h3> Color: ' .$key. ' Showed: '.$val.'</h3>';
+	}		
+	
+	return $result;
+  } 
+
+
+
+function convertImageToBmp($img, $type){
+//Will convert images to bmp
+
+	return $result;
 
 }
 
@@ -52,17 +162,27 @@ $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
-		$check = getimagesize($_FILES["image"]["tmp_name"]);
-		console($check);
-		console($_FILES["image"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-				$uploadOk = 1;
-				dominantColors($_FILES["image"]["tmp_name"]);
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-		}
+
+	console($_FILES["image"]["tmp_name"]);
+
+	$check = getimagesize($_FILES["image"]["tmp_name"]);
+	console($check);
+	
+	if($check !== false) {
+			echo "File is an image - " . $check["mime"] . ".";
+			$imageTemp = $_FILES["image"]["tmp_name"];
+			$uploadOk = 1;
+			if( $check["mime"] === 'image/bmp'){
+				colorsInBmp($imageTemp);
+			} else {
+				$newImage = convertImageToBmp($imageTemp, $check["mime"]);
+				colorsInBmp($newImage);
+			}
+			
+	} else{
+			echo "File is not an image.";
+			$uploadOk = 0;
+	}
 }
 
 	
