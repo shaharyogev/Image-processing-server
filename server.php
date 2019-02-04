bf103311ae9168ae8817e26262945acb7669f7e7
Shahar Yogev <37019685+shaharyogev@users.noreply.github.com>
2019-02-04 09:06:29 +0200

    cleanup

--- server.php
+++ server.php
@@ -34,85 +34,84 @@ $colors = array();
 
 function colorsInBmp($p_sFile) { 
 
-	//    Load the image into a string 
-	$file    =    fopen($p_sFile,"rb"); 
-	$read    =    fread($file,10); 
-	while(!feof($file)&&($read<>"")) 
-			$read    .=    fread($file,1024); 
-	
-	$temp    =    unpack("H*",$read); 
-		console($temp);
+	//Load the image into a string 
+	$file = fopen($p_sFile,"rb"); 
+	$read = fread($file,10); 
 
-	$hex    =    $temp[1]; 
-	$header    =    substr($hex,0,108); 
+	// While the file is open concatenate the next kb and keep it until it ends
+	while(!feof($file)&&($read<>"")) 
+			$read .= fread($file,1024); 
+	
+	//Unpack the read string to hexadecimal string for easy manipulation and reading;
+	$temp = unpack("H*",$read); 
+	$hex = $temp[1]; 
+	// BMP Header is the first 108 characters
+	$header = substr($hex,0,108); 
 	
 	
-	//    Process the header 
-	//    Structure: http://www.fastgraph.com/help/bmp_header_format.html 
+	//Process the header 
+	//Structure: http://www.fastgraph.com/help/bmp_header_format.html 
 	
-	if (substr($header,0,4)=="424d") //BMP header
-	{ 
-			//    Cut it in parts of 2 bytes 
-			$header_parts    =    str_split($header,2); 
+	if (substr($header,0,4) == "424d"){  //BMP header
+			//Cut it in parts of 2 bytes 
+			$header_parts = str_split($header,2); 
 			
-			//    Get the width        4 bytes 
-			$width            =    hexdec($header_parts[19].$header_parts[18]); 
+			//Get the width 4 bytes 
+			$width = hexdec($header_parts[19].$header_parts[18]); 
 			
-			//    Get the height        4 bytes 
-			$height            =    hexdec($header_parts[23].$header_parts[22]); 
+			//Get the height 4 bytes 
+			$height = hexdec($header_parts[23].$header_parts[22]); 
 			
-			//    Unset the header params 
+			//Unset the header params 
 			unset($header_parts); 
 	} 
 	
-	//    Define starting X and Y 
-	$x                =    0; 
-	$y                =    1; 
+	//Define starting X and Y 
+	$x = 0; 
+	$y = 1; 
 	
-	//    Create newimage 
-	//$image            =    imagecreatetruecolor($width,$height); // GD library cant be used
+	// Create newimage 
+	//$image = imagecreatetruecolor($width,$height); // GD library cant be used
 	
-	//    Grab the body from the image 
-	$body            =    substr($hex,108); 
-	//    Calculate if padding at the end-line is needed 
-	//    Divided by two to keep overview. 
-	//    1 byte = 2 HEX-chars 
-	$body_size        =    (strlen($body)/2); 
-	$header_size    =    ($width*$height); 
+	//Grab the body from the image 
+	$body = substr($hex,108); 
+	//Calculate if padding at the end-line is needed 
+	//Divided by two to keep overview. 
+	//1 byte = 2 HEX-chars 
+	$body_size = (strlen($body)/2); 
+	$header_size = ($width*$height); 
 
-	//    Use end-line padding? Only when needed 
-	$usePadding        =    ($body_size>($header_size*3)+4); 
+	//Use end-line padding? Only when needed 
+	$usePadding = ($body_size>($header_size*3)+4); 
 	
-	//    Using a for-loop with index-calculation instated of str_split to avoid large memory consumption 
-	//    Calculate the next DWORD-position in the body 
-	for ($i=0;$i<$body_size;$i+=3) 
-	{ 
-			//    Calculate line-ending and padding 
-			if ($x>=$width) 
-			{ 
-					//    If padding needed, ignore image-padding 
-					//    Shift i to the ending of the current 32-bit-block 
+	//Using a for-loop with index-calculation instated of str_split to avoid large memory consumption 
+	//Calculate the next DWORD-position in the body 
+	for ($i=0;$i<$body_size;$i+=3) { 
+			//Calculate line-ending and padding 
+			if ($x>=$width) { 
+					//If padding needed, ignore image-padding 
+					//Shift i to the ending of the current 32-bit-block 
 					if ($usePadding) 
 							$i    +=    $width%4; 
 					
-					//    Reset horizontal position 
-					$x    =    0; 
+					//Reset horizontal position 
+					$x = 0; 
 					
-					//    Raise the height-position (bottom-up) 
+					//Raise the height-position (bottom-up) 
 					$y++; 
 					
-					//    Reached the image-height? Break the for-loop 
+					//Reached the image-height? Break the for-loop 
 					if ($y>$height) 
 							break; 
 			} 
 							
 
-			//    Calculation of the RGB-pixel (defined as BGR in image-data) 
-			//    Define $i_pos as absolute position in the body 
-			$i_pos    =    $i*2; 
-			$r        =    hexdec($body[$i_pos+4].$body[$i_pos+5]); 
-			$g        =    hexdec($body[$i_pos+2].$body[$i_pos+3]); 
-			$b        =    hexdec($body[$i_pos].$body[$i_pos+1]); 
+			//Calculation of the RGB-pixel (defined as BGR in image-data) 
+			//Define $i_pos as absolute position in the body 
+			$i_pos = $i*2; 
+			$r = hexdec($body[$i_pos+4].$body[$i_pos+5]); 
+			$g = hexdec($body[$i_pos+2].$body[$i_pos+3]); 
+			$b = hexdec($body[$i_pos].$body[$i_pos+1]); 
 			
 			// Combine the RGB to a string
 			$tempString = ''.$r.','.$g.','.$b.'';
@@ -166,7 +165,7 @@ function dominantColors( $img){
 	$body = $temp;
 	//$body = substr($hex,108); 
 	console($body);
-	$body_size = (array_chunk($body)); //test
+	$body_size = (count($body)); //test
 	console($body_size);
 	$x = 0; 
 	$y = 1; 
@@ -179,7 +178,7 @@ function dominantColors( $img){
 	$usePadding = ($body_size>($header_size*3)+4); 
 	
 	for ($i=0;$i<$body_size;$i+=3) { 
-		if ($x>=$width) { 
+		if ($x>=$width) { 	
 				if ($usePadding) 
 						$i+= $width%4; 
 
@@ -265,6 +264,8 @@ if(isset($_POST["submit"])) {
 	
 ?>
 
-	</body>
+<
+/body>
 
-</html>
+<
+/html>
